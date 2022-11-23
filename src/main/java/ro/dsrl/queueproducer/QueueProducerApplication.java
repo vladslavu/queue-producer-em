94242e89c4cbb07890f545437643ce18c2280df1
vlads <vlad.slavu@gmail.com>
2022-11-23 17:22:26 +0200

    generated empty spring project

---
+++ src/main/java/ro/dsrl/queueproducer/QueueProducerApplication.java
@@ -0,0 +1,13 @@
+package ro.dsrl.queueproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/dsrl/queueproducer/QueueProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.dsrl.queueproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
